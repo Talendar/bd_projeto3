1d678ea7717529658d296975cdc4a2f58430b921
Lucas Yamamoto <55724611+lucasyamamoto@users.noreply.github.com>
2021-07-16 19:53:33 -0300

    Fix first query

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -6,7 +6,7 @@ SELECT S.NOME AS SERVICO, A.VALORATIVIDADE AS PRECO, P.NOME AS PRESTADOR, COUNT(
          JOIN PRESTADORSERVICO P ON P.EMAIL = S.PRESTADOR
     WHERE S.PAIS = 'FRANCA'
     GROUP BY S.NOME, A.VALORATIVIDADE, P.NOME
-    ORDER BY NUMEROCONTRATACOES DESC;
+    ORDER BY CONTRATACOES DESC;
 	
 --Consulta 2:
 --Ordenar os prestadores por media de avaliacao dos contratos de servico exibindo o numero de contratos firmados
